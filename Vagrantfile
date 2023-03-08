c94b4a25f955e3772bb74fcd38be317eb0c3f7c8
Krstan Vjestica <krle@tilda.center>
2023-03-08 15:05:01 +0100

    add packages for ubuntu/jammy64 and docker compose

--- Vagrantfile
+++ Vagrantfile
@@ -1,5 +1,7 @@
-Vagrant.configure("2") do |config|
-  config.vm.box = "ubuntu/xenial32"
+Vagrant.configure('2') do |config|
+  config.vm.box = 'ubuntu/jammy64'
+  config.vm.hostname = 'locust'
   config.vm.network :forwarded_port, guest: 8089, host: 8089
-  config.vm.provision :shell, :path => "examples/vagrant/vagrant.sh"
+  config.vm.network 'private_network', type: 'dhcp'
+  config.vm.provision :shell, path: 'examples/vagrant/vagrant.sh'
 end
--- examples/vagrant/vagrant.sh
+++ examples/vagrant/vagrant.sh
@@ -5,14 +5,14 @@
 # 
 
 # Update and install some dependencies
-apt-get -y update 
-apt-get -y install build-essential python-pip python-dev libev-dev libzmq-dev supervisor
+sudo apt-get -y update 
+sudo apt-get -y install build-essential python-is-python3 python-dev-is-python3 python3-pip python3-dev libev-dev libzmq3-dev supervisor
 cd /vagrant
 
-pip install --use-mirrors pyzmq supervisor
+sudo pip install --use-mirrors pyzmq supervisor
 
 # Checkout and install latest Locust from Github
-python setup.py develop
+sudo python setup.py develop
 
 # Starting supervisor which is configured to start Locust
-supervisord -c examples/vagrant/supervisord.conf
+sudo supervisord -c examples/vagrant/supervisord.conf
